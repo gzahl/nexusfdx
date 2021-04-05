2754a22ee60b49a402f221b5ce7d4d7869b0d6f5
Manuel Jung <manuel.jung@hotmail.com>
2021-04-05 18:36:07 +0200

    AIS and GPS working on local network

--- src/main.cpp
+++ src/main.cpp
@@ -11,10 +11,9 @@ SoftwareSerial swSerialNmea1;
 
 void configureUbloxM8Gps();
 
-//unsigned char gpsmessage[100];
-unsigned char gpsmessagelen = 0;
 std::vector<uint8_t> gpsmsg(100);
 std::vector<uint8_t> aismsg(100);
+std::vector<uint8_t> dscmsg(100);
 unsigned char len = 0;
 
 static const bool ENABLE_NMEA2 = false;
@@ -23,8 +22,8 @@ static const bool ENABLE_NMEA0 = true; // AIS Input
 static const bool ENABLE_NMEA1 = true; // DSC Input, GPS Output
 
 
-const char * ssid = "Schmuddelwetter";
-const char * password = "9568164986244857";
+const char* ssid = "Schmuddelwetter_24G";
+const char* password = "9568164986244857";
 
 AsyncUDP udp;
 
@@ -37,6 +36,7 @@ void setup()
 
   aismsg.clear();
   gpsmsg.clear();
+  dscmsg.clear();
    
   Serial.printf("Connecting to Wifi with ssid '%s'.\n", ssid);
   WiFi.mode(WIFI_STA);
@@ -46,6 +46,8 @@ void setup()
         while(1) {
             delay(1000);
         }
+    } else {
+      Serial.println("WiFi connected.");
     }
 
 
@@ -55,7 +57,7 @@ void setup()
   //pinMode(GPIO_NUM_18, OUTPUT);
 
   swSerial.begin(9600, SWSERIAL_8S1, GPIO_NUM_26, GPIO_NUM_18);
-  swSerialGps.begin(4800, SWSERIAL_8N1, GPIO_NUM_23, GPIO_NUM_19);
+  swSerialGps.begin(9600, SWSERIAL_8N1, GPIO_NUM_23, GPIO_NUM_19);
   swSerialNmea0.begin(38400, SWSERIAL_8N1, GPIO_NUM_33, GPIO_NUM_14);
   swSerialNmea1.begin(38400, SWSERIAL_8N1, GPIO_NUM_5, GPIO_NUM_13);
 
@@ -69,8 +71,8 @@ void configureUbloxM8Gps() {
   delay(1000);
   //Serial2.print("$PUBX,41,1,3,2,115200,0*1D\r\n");
   //Serial2.print("$PUBX,41,1,3,2,38400,0*25\r\n");
-  //Serial2.print("$PUBX,41,1,3,2,9600,0*15\r\n");
-  Serial2.print("$PUBX,41,1,3,2,4800,0*16\r\n");
+  Serial2.print("$PUBX,41,1,3,2,9600,0*15\r\n");
+  //Serial2.print("$PUBX,41,1,3,2,4800,0*16\r\n");
   Serial2.flush();
   delay(100);
   Serial2.clearWriteError();
@@ -128,8 +130,9 @@ void loop()
   }
 
     while(ENABLE_NMEA1 && swSerialNmea1.available()) {
-  //  while(swSerialNmea0.available()){
-    byte = swSerialNmea1.read();
-    Serial.write(byte);
+      if(readNmea(dscmsg, swSerialNmea1.read())) {
+        Serial.print(getLine(dscmsg));
+        udp.broadcastTo(getLine(dscmsg), 2000);
+    }
   }
 }
