ece710abec09e76c63abc89d0a14d8953509cbb8
Manuel Jung <manuel.jung@hotmail.com>
2021-07-11 13:22:36 +0200

    More OTA infos.

--- src/main.cpp
+++ src/main.cpp
@@ -97,6 +97,32 @@ void setupApp() {
 
   ArduinoOTA.setHostname(HOST_NAME);
   ArduinoOTA.setMdnsEnabled(true);
+
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH)
+      type = "sketch";
+    else // U_SPIFFS
+      type = "filesystem";
+
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    debugI("OTA start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    debugI("\nOTA End");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    debugI("OTA Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    debugE("OTA Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) debugE("OTA Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) debugE("OTA Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) debugE("OTA Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) debugE("OTA Receive Failed");
+    else if (error == OTA_END_ERROR) debugE("OTA End Failed");
+  });
   ArduinoOTA.begin();
 
 #ifndef DEBUG_DISABLED
